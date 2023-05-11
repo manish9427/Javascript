4192411482e1d63d1fddb2e0e4a29160fa1265a1
manish9427 <manish119427@gmail.com>
2023-05-11 09:24:09 +0530

    closure

--- Concepts/Closure.js
+++ Concepts/Closure.js
@@ -1,8 +1,31 @@
-function Outer(x) {
-  x = 5;
-  function Inner() {
-    console.log(x);
+// function Outer(x) {
+//   var x = 5;
+//   function Inner() {
+//     console.log(x);
+//   }
+// }
+// Outer();
+// Inner();
+
+// function makeCounter() {
+//   let count = 0;
+//   return function () {
+//     count++;
+//     console.log(count);
+//   };
+// }
+
+// let counter = makeCounter();
+// counter(); // logs 1
+// counter(); // logs 2
+
+function outer() {
+  var count = 0;
+  function inner() {
+    count++;
+    console.log(count);
   }
+  return inner();
 }
-Inner();
-Outer();
+
+outer();
--- Concepts/Hoisting.js
+++ Concepts/Hoisting.js
@@ -1,3 +1,4 @@
+console.log(b);
 console.log(a);
 var a = 10;
 console.log(a);
