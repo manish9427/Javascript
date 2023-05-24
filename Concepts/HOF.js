9af934d251b80b21db824b1ffad7afac0f4444e3
manish9427 <manish119427@gmail.com>
2023-05-24 13:51:03 +0530

    arrow

--- Concepts/HOF.js
+++ Concepts/HOF.js
@@ -1,9 +1,12 @@
 console.log("hello form Manish");
 // ----------------------------------forEach------------------------
-const number = [1, 2, 3, 4, 5];
-number.forEach(function (num) {
+const number1 = [1, 2, 3, 4, 5];
+number1.forEach(function (num) {
   console.log(num);
 });
+
+const number2 = [6, 7, 8, 9, 10];
+number2.forEach((num) => console.log(num));
 // ----------------------------------map------------------------
 // ----------------------------------filter------------------------
 // ----------------------------------reducer------------------------
