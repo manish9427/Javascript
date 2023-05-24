279052739d3b18a5f9232e2422e65c2ebf3c2cc0
manish9427 <manish119427@gmail.com>
2023-05-24 14:24:53 +0530

    filter

--- Concepts/HOF.js
+++ Concepts/HOF.js
@@ -8,5 +8,14 @@ number1.forEach(function (num) {
 const number2 = [6, 7, 8, 9, 10];
 number2.forEach((num) => console.log(num));
 // ----------------------------------map------------------------
+
+const number3 = [1, 2, 3];
+const ans = number3.map((num) => num);
+console.log(ans);
 // ----------------------------------filter------------------------
+
+const number4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const answer = number4.filter((x) => x % 2 === 0);
+console.log(answer);
+
 // ----------------------------------reducer------------------------
