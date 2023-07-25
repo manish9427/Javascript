496aee835550e537fca7e3ede42a39a4b3ad10ce
manish9427 <manish119427@gmail.com>
2023-07-25 13:34:23 +0530

    new

---
+++ Target/YouTube/Hoisting.js
@@ -0,0 +1,20 @@
+// --------------Variable----------------------
+// var a = 10;
+// console.log("first Output " + a);
+
+// var b;
+// console.log("Second Output " + b);
+
+// console.log("Third Output " + c);   --> c is not defined
+
+// console.log("output " + d);
+// var d = 10;
+// console.log("output " + d);
+
+// Hoisting is a default behavior in JavaScript which moves the variable and function declaration
+// to the top of the scope
+
+// scopes
+// block
+// gobal
+// functional
