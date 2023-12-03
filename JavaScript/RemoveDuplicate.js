fe74619fd35056e91b0686c48c2dafac4edbf7f1
manish9427 <manish119427@gmail.com>
2023-12-03 19:00:14 +0530

    new

---
+++ JavaScript/RemoveDuplicate.js
@@ -0,0 +1,23 @@
+// const fruits = ["apple", "mango", "grapes", "apple", "mango", "grapes"];
+
+// const fun = () => {
+//   for (let i = 0; i < fruits.length - 1; j++) {
+//     for (let j = i + 1; j < fruits.length; j++) {
+//       if (fruits[i] === fruits[j]) {
+//       }
+//     }
+//   }
+// };
+
+// console.log(fun());
+
+const fruits = ["apple", "mango", "grapes", "apple", "mango", "grapes"];
+
+// Convert the array to a Set to remove duplicates
+const uniqueFruitsSet = new Set(fruits);
+
+// Convert the Set back to an array
+// const uniqueFruitsArray = Array.from(uniqueFruitsSet);
+const uniqueFruitsArray = [...uniqueFruitsSet];
+
+console.log(uniqueFruitsArray);
