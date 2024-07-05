8b01e1698e920cfa178b7d2398948a1f11695e08
manish9427 <manish119427@gmail.com>
2024-07-05 23:27:26 +0530

    new

--- JavaScript/closure.js
+++ JavaScript/closure.js
@@ -1,1 +1,12 @@
  // Closure is a feture in JS hich enable user to use variable and datatype of outer funtion 
+
+ const fun=()=> {
+    let a=10;
+    return () => {
+        console.log(a);
+    }
+ }
+
+ 
+ const myClosure = fun();
+ myClosure();
