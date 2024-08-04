6ea71006653353fa5682161fadf2a56db41cb8d7
manish9427 <manish119427@gmail.com>
2024-08-04 23:33:49 +0530

    new

--- JavaScript/Input/Input.js
+++ JavaScript/Input/Input.js
@@ -2,5 +2,12 @@ const readline = require("readline");
 const rl = readline.createInterface({input:process.stdin,output:process.stdout})
 rl.question("Enter the Name: ",(userInput)=>{
     console.log("User Input : ",userInput);
+    let a = userInput;
     rl.close();
 })
+
+
+//Print Element
+for(let i=0;i<a.length;i++){
+    console.log(i);
+}
