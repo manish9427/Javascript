cab0ed62d8fae0f19c5ada5fbccd578ce800a722
manish9427 <manish119427@gmail.com>
2024-10-03 11:28:36 +0530

    fibonacci

--- Programming/FibonacciSeries.js
+++ Programming/FibonacciSeries.js
@@ -1,7 +1,17 @@
 const readline = require("readline");
 const rl = readline.createInterface({input:process.stdin,output:process.stdout});
 rl.question("Enter the number: ",(num)=>{
-    console.log(num)
+    let n = parseInt(num);
+    let first = 0;
+    let second = 1;
+    let fibonacci = [first,second];
+    for(let i =2 ;i<n;i++){
+        let next = first + second;
+        fibonacci.push(next);
+        first = second;
+        second = next;
+    }
+    console.log(fibonacci);
     rl.close();
 });
 
