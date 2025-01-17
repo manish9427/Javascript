9748e85f374c999f15fad57445ea26c9b27519da
manish9427 <manish119427@gmail.com>
2025-01-18 02:22:41 +0530

    new

--- Programming/SubString.js
+++ Programming/SubString.js
@@ -5,8 +5,8 @@ const subString = function(str){
     let right = str.length-1;
     for(let i=0;i<str.length;i++){
         for(let j=i+1;j<=str.length;j++){
-            let tring = str.slice(i,j);
-            console.log(tring);
+            let s = str.slice(i,j);
+            console.log(s);
         }
     }
 }
