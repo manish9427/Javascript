a8a6ade187013c08f0314323241ffc94aca7b3e2
manish9427 <manish119427@gmail.com>
2025-02-02 19:37:16 +0530

    new

--- JavaScript/Input/Input.js
+++
@@ -1,9 +0,0 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Enter the Name: ", (userInput) => {
-  console.log("User Input: ", userInput);
-});
