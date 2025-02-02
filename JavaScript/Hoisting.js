fc77df108c17d194e59e41367b46f13154deb8b4
manish9427 <manish119427@gmail.com>
2025-02-02 19:34:36 +0530

    new

---
+++ JavaScript/Hoisting.js
@@ -0,0 +1,10 @@
+a=10;
+console.log(a);
+var a;
+
+
+const b =10;
+const fun = function(){
+    console.log(b);   
+} 
+fun()
--- JavaScript/Input/Input.js
+++ JavaScript/Input/Input.js
@@ -1,13 +1,9 @@
 const readline = require("readline");
-const rl = readline.createInterface({input:process.stdin,output:process.stdout})
-rl.question("Enter the Name: ",(userInput)=>{
-    console.log("User Input : ",userInput);
-    let a = userInput;
-    rl.close();
-})
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-
-//Print Element
-for(let i=0;i<a.length;i++){
-    console.log("ans: "+ i+1);
-}
+rl.question("Enter the Name: ", (userInput) => {
+  console.log("User Input: ", userInput);
+});
